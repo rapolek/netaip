6bb23288069daf959ea6ac3944d9cf89b587aa4e
rapolek <57568910+rapolek@users.noreply.github.com>
2020-08-11 12:24:08 -0700

    add loves on posts

--- src/templates/home.js
+++ src/templates/home.js
@@ -1,72 +1,139 @@
-import React from 'react';
-import _ from 'lodash';
-import moment from 'moment-strftime';
-import {graphql} from 'gatsby';
+import React from "react";
+import _ from "lodash";
+import moment from "moment-strftime";
+import { graphql } from "gatsby";
 
-import {Layout} from '../components/index';
-import {markdownify, Link, withPrefix, classNames, getPages} from '../utils';
+import { Layout } from "../components/index";
+import { markdownify, Link, withPrefix, classNames, getPages } from "../utils";
 
 // this minimal GraphQL query ensures that when 'gatsby develop' is running,
 // any changes to content files are reflected in browser
 export const query = graphql`
   query($url: String) {
-    sitePage(path: {eq: $url}) {
+    sitePage(path: { eq: $url }) {
       id
     }
   }
 `;
 
-export default class Home extends React.Component {
-    render() {
-        let display_posts = _.orderBy(getPages(this.props.pageContext.pages, '/posts'), 'frontmatter.date', 'desc');
-        return (
-            <Layout {...this.props}>
-              {_.get(this.props, 'pageContext.frontmatter.has_intro', null) && (
-              <div className="intro">
-                <div className="inner-md">
-                  {_.get(this.props, 'pageContext.frontmatter.intro_content', null) && (
-                  <div className="intro-text">
-                    {markdownify(_.get(this.props, 'pageContext.frontmatter.intro_content', null))}
-                  </div>
+const Home = (props) => {
+  const [loves, setLoves] = React.useState([
+    { id: "basic-rules-for-walking-in-the-mountains", loves: 3 },
+  ]);
+
+  function addLove(id) {
+    const newLoves = loves.map((page) => {
+      if (page.id === id) {
+        return { ...page, loves: page.loves + 1 };
+      }
+
+      return page;
+    });
+
+    setLoves(newLoves);
+  }
+
+  function removeLove(id) {
+    setLoves(loves - 1);
+  }
+
+  let display_posts = _.orderBy(
+    getPages(props.pageContext.pages, "/posts"),
+    "frontmatter.date",
+    "desc"
+  );
+  console.log(display_posts);
+  return (
+    <Layout {...props}>
+      {_.get(props, "pageContext.frontmatter.has_intro", null) && (
+        <div className="intro">
+          <div className="inner-md">
+            {_.get(props, "pageContext.frontmatter.intro_content", null) && (
+              <div className="intro-text">
+                {markdownify(
+                  _.get(props, "pageContext.frontmatter.intro_content", null)
+                )}
+              </div>
+            )}
+            {_.get(props, "pageContext.frontmatter.intro_actions", null) && (
+              <div className="intro-cta">
+                {_.map(
+                  _.get(props, "pageContext.frontmatter.intro_actions", null),
+                  (action, action_idx) => (
+                    <Link
+                      key={action_idx}
+                      to={withPrefix(_.get(action, "url", null))}
+                      className={classNames({
+                        button:
+                          _.get(action, "style", null) === "primary" ||
+                          _.get(action, "style", null) === "secondary",
+                        "button-secondary":
+                          _.get(action, "style", null) === "secondary",
+                      })}
+                      {...(_.get(action, "new_window", null)
+                        ? { target: "_blank", rel: "noopener" }
+                        : null)}
+                    >
+                      {_.get(action, "label", null)}
+                    </Link>
+                  )
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+      <div className="post-feed">
+        {_.map(display_posts, (post, post_idx) => (
+          <article key={post_idx} className="post post-card">
+            <div className="post-card-inside">
+              {_.get(post, "frontmatter.thumb_img_path", null) && (
+                <img
+                  className="thumbnail"
+                  src={withPrefix(
+                    _.get(post, "frontmatter.thumb_img_path", null)
                   )}
-                  {_.get(this.props, 'pageContext.frontmatter.intro_actions', null) && (
-                  <div className="intro-cta">
-                    {_.map(_.get(this.props, 'pageContext.frontmatter.intro_actions', null), (action, action_idx) => (
-                    <Link key={action_idx} to={withPrefix(_.get(action, 'url', null))} className={classNames({'button': (_.get(action, 'style', null) === 'primary') || (_.get(action, 'style', null) === 'secondary'), 'button-secondary': _.get(action, 'style', null) === 'secondary'})} {...(_.get(action, 'new_window', null) ? ({target: '_blank', rel: 'noopener'}) : null)}>{_.get(action, 'label', null)}</Link>
-                    ))}
-                  </div>
+                  alt={_.get(post, "frontmatter.title", null)}
+                />
+              )}
+              <div className="post-card-content">
+                <header className="post-header">
+                  <div className="post-meta"></div>
+                  <h2 className="post-title">
+                    <Link
+                      to={withPrefix(_.get(post, "url", null))}
+                      rel="bookmark"
+                    >
+                      {_.get(post, "frontmatter.title", null)}
+                    </Link>
+                  </h2>
+                </header>
+                <div className="post-excerpt">
+                  {_.get(post, "frontmatter.excerpt", null) && (
+                    <p>
+                      <Link
+                        to={withPrefix(_.get(post, "url", null))}
+                        rel="bookmark"
+                      >
+                        {_.get(post, "frontmatter.excerpt", null)}
+                      </Link>
+                    </p>
                   )}
                 </div>
-              </div>
-              )}
-              <div className="post-feed">
-                {_.map(display_posts, (post, post_idx) => (
-                <article key={post_idx} className="post post-card">
-                  <div className="post-card-inside">
-                    {_.get(post, 'frontmatter.thumb_img_path', null) && (
-                      <img className="thumbnail" src={withPrefix(_.get(post, 'frontmatter.thumb_img_path', null))} alt={_.get(post, 'frontmatter.title', null)} />
-                    )}
-                    <div className="post-card-content">
-                      <header className="post-header">
-                        <div className="post-meta">
-                        </div>
-                        <h2 className="post-title"><Link to={withPrefix(_.get(post, 'url', null))} rel="bookmark">{_.get(post, 'frontmatter.title', null)}</Link></h2>
-                      </header>
-                      <div className="post-excerpt">
-                        {_.get(post, 'frontmatter.excerpt', null) && (
-                        <p>
-                          <Link to={withPrefix(_.get(post, 'url', null))} rel="bookmark">
-                            {_.get(post, 'frontmatter.excerpt', null)}
-                          </Link>
-                        </p>
-                        )}
-                      </div>
-                    </div>
+                <div className="love">
+                  <button onClick={() => addLove(post.name)}>luv</button>
+                  <div className="how-many-people-liked-this">
+                    {loves.find((page) => page.id === post.name)?.loves} people
+                    liked this
                   </div>
-                </article>
-                ))}
+                </div>
               </div>
-            </Layout>
-        );
-    }
-}
+            </div>
+          </article>
+        ))}
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
