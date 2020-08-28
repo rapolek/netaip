6d97af8a4889e0ea8e3b8fa8f2dafa3b5ecf207c
rapolek <57568910+rapolek@users.noreply.github.com>
2020-08-27 19:00:26 -1200

    fonts 2.0

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -16,8 +16,7 @@ export default class Body extends React.Component {
                     <meta charSet="utf-8"/>
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="description" content={_.get(this.props, 'pageContext.frontmatter.excerpt', null) || _.get(this.props, 'pageContext.site.siteMetadata.description', null)}/>
-                    <link href="https://fonts.googleapis.com/css?family=PT+Serif:400,700%7CRoboto:400,400i,700,700i&display=swap" rel="stylesheet"/> 
-                    <link rel="stylesheet" href={withPrefix('assets/css/main.css')}/>
+                    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400&family=Roboto:ital@1&display=swap" rel="stylesheet"/>                    <link rel="stylesheet" href={withPrefix('assets/css/main.css')}/>
                     {(_.get(this.props, 'pageContext.frontmatter.template', null) === 'post') && ( 
                     _.get(this.props, 'pageContext.frontmatter.canonical_url', null) && (
                     <link rel="canonical" href={_.get(this.props, 'pageContext.frontmatter.canonical_url', null)}/>
